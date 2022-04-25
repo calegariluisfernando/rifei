3d1bd6c5201b5c3013074d9d02f10317e6576a5d
Luis Fernando Calegari <calegariluisfernando@gmail.com>
2022-04-24 23:14:28 -0300

    Criando Imagem Docker

---
+++ package.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define packaging environment.
+#export ETC_DIR=./backend/etc
+
+# Define RASP configuration.
+#cp $ETC_DIR/contrast_security.yaml.template $ETC_DIR/contrast_security.yaml
+
+#sed -i -e 's|${CONTRAST_API_KEY}|'"$CONTRAST_API_KEY"'|g' $ETC_DIR/contrast_security.yaml
+#sed -i -e 's|${CONTRAST_SERVICE_KEY}|'"$CONTRAST_SERVICE_KEY"'|g' $ETC_DIR/contrast_security.yaml
+#sed -i -e 's|${CONTRAST_USER_NAME}|'"$CONTRAST_USER_NAME"'|g' $ETC_DIR/contrast_security.yaml
+
+# Define packaging environment.
+echo "REPOSITORY_URL=$REPOSITORY_URL" > ./iac/.env
+echo "REPOSITORY_ID=$REPOSITORY_ID" >> ./iac/.env
+
+# Build docker images.
+docker-compose -f ./iac/docker-compose.yml build
+
+# Save images locally.
+rm -f /tmp/rifei-*.tar
+
+docker save $REPOSITORY_ID/rifei-frontend:latest -o /tmp/rifei-frontend.tar
+
+# Clean temporary files.
+#rm $ETC_DIR/contrast_security.yaml
---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Authenticate in the packaging repository.
+echo $REPOSITORY_PASSWORD | docker login -u $REPOSITORY_USER $REPOSITORY_URL --password-stdin
+
+# Tag the version in the packages and push into the repository
+BUILD_VERSION=$(md5sum -b /tmp/refei-frontend.tar | awk '{print $1}')
+docker tag $REPOSITORY_URL/$REPOSITORY_ID/refei-frontend:latest $REPOSITORY_URL/$REPOSITORY_ID/refei-frontend:$BUILD_VERSION
+docker push $REPOSITORY_URL/$REPOSITORY_ID/refei-frontend:$BUILD_VERSION
+
+docker-compose -f ./iac/docker-compose.yml push
