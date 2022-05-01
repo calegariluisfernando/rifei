ee89b5ab180ff2fe2d77fc09992211ca10b78921
Luis Fernando Calegari <calegariluisfernando@gmail.com>
2022-05-01 13:58:45 -0300

    Criando imagem Docker no Docker Hub

--- publish.sh
+++ publish.sh
@@ -4,9 +4,9 @@
 # Authenticate in the packaging repository.
 echo $REPOSITORY_PASSWORD | docker login -u $REPOSITORY_USER $REPOSITORY_URL --password-stdin
 
-# # Tag the version in the packages and push into the repository
-# BUILD_VERSION=$(md5sum -b /tmp/rifei-frontend.tar | awk '{print $1}')
-# docker tag $REPOSITORY_URL/$REPOSITORY_ID/rifei-frontend:latest $REPOSITORY_URL/$REPOSITORY_ID/rifei-frontend:$BUILD_VERSION
-# docker push $REPOSITORY_URL/$REPOSITORY_ID/rifei-frontend:$BUILD_VERSION
+# Tag the version in the packages and push into the repository
+BUILD_VERSION=$(md5sum -b /tmp/rifei-frontend.tar | awk '{print $1}')
+docker tag $REPOSITORY_URL/$REPOSITORY_ID/rifei-frontend:latest $REPOSITORY_URL/$REPOSITORY_ID/rifei-frontend:$BUILD_VERSION
+docker push $REPOSITORY_URL/$REPOSITORY_ID/rifei-frontend:$BUILD_VERSION
 
-# docker-compose -f ./iac/docker-compose.yml push
+docker-compose -f ./iac/docker-compose.yml push
